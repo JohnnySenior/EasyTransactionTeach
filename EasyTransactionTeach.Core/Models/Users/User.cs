c153965864cb7dc53eadb404fea1295c78ec2b50
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-01 18:26:22 +0900

    DATA: Create User Model

---
+++ EasyTransactionTeach.Core/Models/Users/User.cs
@@ -0,0 +1,17 @@
+﻿//==========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//==========================
+
+namespace EasyTransactionTeach.Core.Models.Users
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Nationality { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
