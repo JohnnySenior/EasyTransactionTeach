897e161fa4f9a5f324086eee8aa769933cfdee50
Zafar <icom@MACKBOOK>
2023-10-01 14:47:26 +0300

    DATA: Create Account Model

---
+++ EasyTransactionTeach.Core/Models/Accounts/Account.cs
@@ -0,0 +1,18 @@
+﻿//==========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//==========================
+
+using System;
+
+namespace EasyTransactionTeach.Core.Models.Accounts
+{
+    internal class Account
+    {
+        public Guid Id { get; set; }
+        public Guid AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
