2d8909dffedaf18a6d864bee4d8833b8aece14f7
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-01 18:14:39 +0900

    INFRA: Initialize Console Project

---
+++ EasyTransactionTeach.Core/Program.cs
@@ -0,0 +1,15 @@
+﻿//==========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//==========================
+
+namespace EasyTransaction.Core
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
