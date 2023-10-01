dd21931d02adb9d6b6d48d48c8e9e1fffa7346fb
Zafar <icom@MACKBOOK>
2023-10-01 17:52:01 +0300

    BROKERS: Create Logging

---
+++ EasyTransactionTeach.Core/Brokers/Loggings/Logging/LoggingBroker.cs
@@ -0,0 +1,15 @@
+﻿//==========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//==========================
+
+using System;
+
+namespace EasyTransactionTeach.Core.Brokers.Loggings.Logging
+{
+    internal class LoggingBroker
+    {
+        public void LogError(Exception exception) =>
+            Console.WriteLine(exception.Message);
+    }
+}
