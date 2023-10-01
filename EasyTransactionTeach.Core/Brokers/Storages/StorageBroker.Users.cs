27b7f939ba078dde68110231ecd28ba04bd81d1e
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-01 20:37:45 +0900

    BROKERS: Insert User

--- EasyTransactionTeach.Core/Brokers/Storages/StorageBroker.Users.cs
+++ EasyTransactionTeach.Core/Brokers/Storages/StorageBroker.Users.cs
@@ -3,6 +3,7 @@
 // Manage Your Money Easy
 //==========================
 
+using System.Threading.Tasks;
 using EasyTransactionTeach.Core.Models.Users;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,5 +12,13 @@ namespace EasyTransactionTeach.Core.Brokers.Storages
     internal partial class StorageBroker
     {
         public DbSet<User> Users { get; set; }
+
+        public async Task<User> InsertUserAsync(User user)
+        {
+            await this.Users.AddAsync(user);
+            await this.SaveChangesAsync();
+
+            return user;
+        }
     }
 }
